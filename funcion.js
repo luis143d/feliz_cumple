bc8d83303b980c80e9d498788c11265cba2869d2
luis143d <108910590+luis143d@users.noreply.github.com>
2023-06-25 22:01:50 -0500

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      variableWidth: true,
+      arrows: false,
+      pauseOnHover: false,
+      pauseOnFocus: false
+    });
+  });
+  
